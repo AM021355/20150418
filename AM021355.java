39b57e49408087f03df403a15406fec705abea80
AM021355 <a0912481437@gmail.com>
2015-04-18 15:55:27 +0800

    develope

---
+++ AM021355.java
@@ -0,0 +1,13 @@
+import java.until.Scanner;
+
+class am021355
+{
+	public void Robert()
+	{
+  		Scanner input =new Scanner(System.in);
+  		int x=input.nextInt();
+  		int y=input.nextInt();
+		
+		System.out.println(x+y);
+   }
+}
